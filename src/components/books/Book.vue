7ac285b9a9292502c9b53b2af04f86bdeb9b47e5
Alejandro Bonilla <abonilla@edutecno.com>
2020-10-17 12:05:20 -0300

    clase1 frameworks

--- src/components/books/Book.vue
+++ src/components/books/Book.vue
@@ -1,6 +1,97 @@
 <template>
   <div>
-    <img :src="form.image" width="300" alt="img">
+    <h2>{{ form.title }}</h2>
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col md="12">
+          <b-form-checkbox v-model="edit" name="check-button" switch>
+            Editar libro
+          </b-form-checkbox>
+        </b-col>
+        <b-col md="4" class="mr-5">
+          <img :src="form.image" width="350" alt="img" />
+        </b-col>
+        <b-col md="6">
+          <div>
+          <b-form>
+            <b-form-group
+              id="input-group-1"
+              label="Titulo:"
+              label-for="input-1"
+            >
+              <b-form-input
+                id="input-1"
+                v-model="form.title"
+                type="text"
+                required
+                placeholder="Ingresar Titulo"
+                :disabled="!edit"
+              ></b-form-input>
+            </b-form-group>
+
+            <b-form-group
+              id="input-group-2"
+              label="Autor:"
+              label-for="input-2"
+            >
+              <b-form-input
+                id="input-2"
+                v-model="form.author"
+                required
+                placeholder="Ingresar Autor"
+                :disabled="!edit"
+              ></b-form-input>
+            </b-form-group>
+
+            <b-form-group
+              id="input-group-3"
+              label="Url Imagen:"
+              label-for="input-3"
+            >
+              <b-form-input
+                id="input-3"
+                v-model="form.image"
+                required
+                placeholder="Ingresar url"
+                :disabled="!edit"
+              ></b-form-input>
+            </b-form-group>
+
+            <b-form-group
+              id="input-group-4"
+              label="Paginas:"
+              label-for="input-4"
+            >
+              <b-form-input
+                id="input-4"
+                v-model="form.pages"
+                required
+                placeholder="Ingresar numero paginas"
+                :disabled="!edit"
+              ></b-form-input>
+            </b-form-group>
+
+            <b-form-group id="input-group-5" label="Tipo:" label-for="input-5">
+              <b-form-select
+                id="input-5"
+                v-model="form.type"
+                :options="types"
+                required
+                :disabled="!edit"
+              ></b-form-select>
+            </b-form-group>
+
+           
+
+            <b-button type="submit" variant="primary" @click.prevent="editBook" :disabled="!edit">Editar</b-button>
+            <b-button type="reset" variant="danger" @click.prevent="destroyBook">Eliminar</b-button>
+          </b-form>
+          </div>
+        </b-col>
+        
+      </b-row>
+    </b-container>
+    <!-- <img :src="form.image" width="300" alt="img">
     <form>
       <label for="">Editar</label>
       <input type="checkbox" v-model="edit"><br>
@@ -16,86 +107,88 @@
       </select><br>
       <button @click.prevent="editBook" :disabled="!edit">Editar</button>
       <button @click.prevent="destroyBook">Eliminar</button>
-    </form>
+    </form> -->
   </div>
 </template>
 
 <script>
-import {mapGetters, mapState, mapActions} from 'vuex'
+import { mapGetters, mapState, mapActions } from "vuex";
 export default {
-  name: 'book-component',
-  props: ['id'],
-  data: function(){
+  name: "book-component",
+  props: ["id"],
+  data: function() {
     return {
-      form:{
-        title:"",
-        author:"",
-        pages:"",
-        type:"",
-        image:"",
+      form: {
+        title: "",
+        author: "",
+        pages: "",
+        type: "",
+        image: "",
       },
-      edit:false
-    }
+      edit: false,
+    };
   },
   computed: {
-    ...mapGetters(['getBook']),
-    ...mapState(['types'])
+    ...mapGetters(["getBook"]),
+    ...mapState(["types"]),
   },
   methods: {
     // -- Metodos
-    async setBook(){
-      let book = this.getBook(this.id)
-      console.log(book)
-      if(book === undefined){
-        let resp = await this.fetchIdBook(this.id)
-        book = resp.data()
+    async setBook() {
+      let book = this.getBook(this.id);
+      console.log(book);
+      if (book === undefined) {
+        let resp = await this.fetchIdBook(this.id);
+        book = resp.data();
       }
 
-      this.form.title = book.title
-      this.form.author = book.author
-      this.form.type = book.type
-      this.form.image = book.image
-      this.form.pages = book.pages
+      this.form.title = book.title;
+      this.form.author = book.author;
+      this.form.type = book.type;
+      this.form.image = book.image;
+      this.form.pages = book.pages;
     },
-    editBook(){
-      let book = this.form
-      book.id = this.id
+    editBook() {
+      let book = this.form;
+      book.id = this.id;
 
-      let response = this.updateBook(book)
+      let response = this.updateBook(book);
 
-      response.then(()=>{
-        alert('Libro actualizado')
-        this.$router.push("/libros")
-      }).catch(error=>{
-        console.log(error)
-      })
+      response
+        .then(() => {
+          alert("Libro actualizado");
+          this.$router.push("/libros");
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     },
-    destroyBook(){
-      let res = confirm("¿Estas seguro de querer borrar?")
+    destroyBook() {
+      let res = confirm("¿Estas seguro de querer borrar?");
 
-      if(!res) return //guardia
+      if (!res) return; //guardia
 
-      this.deleteBook(this.id).then(()=>{
-        alert("libro Eliminado");
-        this.$router.push("/libros")
-      }).catch(error=>{
-        console.log(error)
-      })
-      
-
+      this.deleteBook(this.id)
+        .then(() => {
+          alert("libro Eliminado");
+          this.$router.push("/libros");
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     },
-    ...mapActions(['fetchIdBook','updateBook','deleteBook'])
+    ...mapActions(["fetchIdBook", "updateBook", "deleteBook"]),
   },
   // components: {},
-  created(){
+  created() {
     this.setBook();
-  }
-}
+  },
+};
 </script>
 
 <style scoped>
-  form{
-    display:inline-block;
-    vertical-align: top;
-  }
+/* form {
+  display: inline-block;
+  vertical-align: top;
+} */
 </style>
--- src/views/Books.vue
+++ src/views/Books.vue
@@ -1,47 +1,75 @@
 <template>
   <div>
     <h1>Libros</h1>
-    <ul>
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col md="4"  v-for="book in books" :key="book.id">
+          <b-card
+            :title="book.title"
+            :img-src="book.image"
+            img-alt="Image"
+            img-top
+            tag="article"
+            style="max-width: 400px;"
+            class="mb-2"
+          >
+            
+
+            <b-button href="#" variant="primary" @click="redirect(book.id)">Ver descripción</b-button>
+          </b-card>
+        </b-col>
+        <b-col>2 of 3</b-col>
+        <b-col>3 of 3</b-col>
+      </b-row>
+    </b-container>
+    <!-- <ul>
       <li v-for="book in books" :key="book.id" class="book">
         <img :src="book.image" width="200" alt="img"><br>
         <span>Titulo: <router-link :to="`/libros/${book.id}`">{{book.title}}</router-link></span>
       </li>
-    </ul>
+    </ul> -->
   </div>
 </template>
 
 <script>
-import {mapActions, mapState} from 'vuex'
+import { mapActions, mapState } from "vuex";
 export default {
-  name: 'books-gallery',
+  name: "books-gallery",
   // props: {},
-  data: function(){
-    return {
-
-    }
+  data: function() {
+    return {};
   },
   computed: {
-    ...mapState(['books'])
+    ...mapState(["books"]),
   },
   methods: {
     // -- Metodos
-    ...mapActions(['fetchBooks'])
+    redirect(book_id){
+      this.$router.push(`/libros/${book_id}`)
+    },
+    ...mapActions(["fetchBooks"]),
   },
   // components: {},
-  created(){
+  created() {
     this.fetchBooks();
-  }
-}
+  },
+};
 </script>
 
 <style scoped>
-  li{
-    display:block;
-  }
+li {
+  display: block;
+}
 
-  .book{
-    display:inline-block;
-    width: 200px;
-    padding: 20px;
-  }
+.book {
+  display: inline-block;
+  width: 200px;
+  padding: 20px;
+}
+
+  .card-img-top{
+  width: 100%;
+  height: 400px;
+  object-fit: cover;
+}
 </style>
